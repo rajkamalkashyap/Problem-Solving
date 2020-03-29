fed8a0a4687891a15c5c87d2ded20b511d7ce991
Raj kamal <rajkamalkashyap@gmail.com>
2020-03-29 12:08:34 +0530

    Update SquareRoot.java

--- SquareRoot.java
+++ SquareRoot.java
@@ -1,40 +1,40 @@
-package gs;
- // https://www.geeksforgeeks.org/square-root-of-a-perfect-square/
-// Babylonian method  
-/*1 Start with an arbitrary positive start value x (the closer to the 
-		   root, the better).
-		2 Initialize y = 1.
-		3. Do following until desired approximation is achieved.
-		  a) Get the next approximation for root using average of x and y
-		  b) Set y = n/x*/
-public class SquareRoot {
-	/*
-	 * Returns the square root of n. Note that the function
-	 */
-	static float squareRoot(float n) {
+package gs.practiceCode;
 
-		/*
-		 * We are using n itself as initial approximation This can definitely be
-		 * improved
-		 */
-		float x = n;
-		float y = 1;
+public class Solution {
+	/*
+	 * double squareRoot( double x )
+	 *
+	 */
+
+	public static double squareRoot(double n) {
+	//  We are using n itself as initial approximation This can definitely be
+		 // improved
+		 
+		double x = n;
+		double y = 1;
 
 		// e decides the accuracy level
 		double e = 0.000001;
 		while (x - y > e) {
 			x = (x + y) / 2;
 			y = n / x;
+			//System.out.println("x:"+x+" y:"+y);
 		}
 		return x;
+	
 	}
+	
 
-	/*
-	 * Driver program to test above function
-	 */
-	public static void main(String[] args) {
-		int n = 2;
-
-		System.out.printf("Square root of " + n + " is " + squareRoot(n));
+	public static void main(String args[]) {
+		double[] inputs = { 2, 4, 100 };
+		double[] expected_values = { 1.41421, 2, 10 };
+		double threshold = 0.001;
+		for (int i = 0; i < inputs.length; i++) {
+			if (Math.abs(squareRoot(inputs[i]) - expected_values[i]) > threshold) {
+				System.out.printf("Test failed for %f, expected=%f, actual=%f\n", inputs[i], expected_values[i],
+						squareRoot(inputs[i]));
+			}
+		}
+		System.out.println("All tests passed");
 	}
 }
