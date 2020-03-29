949c9095af19d40cd12ae932cdde046d138e5d27
Raj kamal <rajkamalkashyap@gmail.com>
2020-03-29 11:57:53 +0530

    Update PascalTraingleMyCode.java

--- PascalTraingleMyCode.java
+++ PascalTraingleMyCode.java
@@ -1,28 +1,49 @@
-package gs;
+package gs.practiceCode;
 
-import java.util.Arrays;
+/*
+**  Below formation is called Pascals Triangle.
+**
+**  Example:
+**               1
+**              1 1
+**             1 2 1
+**            1 3 3 1
+**           1 4 6 4 1
+**         1 5 10 10 5 1
+**        1 6 15 20 15 6 1
+**
+**  Complete the 'pascal' function below so that given a
+**  col and a row it will return the value in that positon.
+**
+**  Example, pascal(1,2) should return 2
+**
+*/
 
-public class PascalTraingleMyCode {
+/* 
+*					******** IMPORTANT ********
+*
+* THIS IS SAMPLE SOLUTION. IF YOU FIND BETTER SOLUTION PLEASE CONSIDER USING SAME.
+* USE YOUR OWN VARIABLE NAMES - @@@ DO NOT COPY @@@ EXACT VARIABLE NAMES
+*
+*/
+import java.util.HashMap;
+import java.util.Map;
 
-	public static void main(String[] args) {
-		/*int k = 9;
-		int arr[][] = new int[k][k];
+public class Solution {
 
-		int ans[][] = constructPascal(arr, k);
-		System.out.println(arr[2][1]);*/
-		int ans=pascal(4,8);
-		System.out.println(ans);
-	}
-	
-	public static  int pascal(int col, int row){
+  public static  int pascal(int col, int row){
+	  	if(row ==0 && col == 0)
+	  		return 1;
+	  	else {	
 		int k=Math.max(col, row);
 		int arr[][] = new int[k+1][k+1];
 
 		int ans[][] = constructPascal(arr, k+1);
 		return ans[row][col];
-	}
+	  	}
 	
-	private static int[][] constructPascal(int[][] arr, int k) {
+  }
+  private static int[][] constructPascal(int[][] arr, int k) {
 
 		arr[0][0] = 1;
 		for (int ii = 0; ii < k; ii++)
@@ -37,5 +58,18 @@ public class PascalTraingleMyCode {
 		}
 		return arr;
 	}
-
+  public static void main(String[] args) {
+	  if(Solution.pascal(0,0) ==  1 &&
+			  Solution.pascal(1,2) ==  2 &&
+			  Solution.pascal(1,1) ==  1 &&
+			  Solution.pascal(0,1) ==  1 &&
+			  Solution.pascal(2,2) ==  1 &&
+			  Solution.pascal(5,6) ==  6 &&
+			  Solution.pascal(4,8) ==  70 &&
+			  Solution.pascal(6,6) ==  1) {
+		  System.out.println("Pass");		  
+	  }else {
+		  System.out.println("Failed");
+	  }
+  }
 }
