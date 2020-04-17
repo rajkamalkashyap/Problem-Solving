03d6c9e39fee406c4bc38746d9eb40eb8350e95a
Raj kamal <rajkamalkashyap@gmail.com>
2020-04-17 09:58:10 +0530

    Update PrimeFactorization.java

--- PrimeFactorization.java
+++ PrimeFactorization.java
@@ -5,14 +5,20 @@ import java.util.ArrayList;
 public class PrimeFactorization {
 
 	public static void main(String args[]) {
-		primeFactors(1575);
+//		ArrayList ans=primeFactors(1575);
+//		System.out.println(ans);
+		ArrayList ans=primeFactors(15);
+		System.out.println(ans);
+		//primeFactors(6);
 		// System.out.println(ans);
 	}
 
-	public static void primeFactors(int n) {
+	public static ArrayList<Integer> primeFactors(int n) {
+		ArrayList<Integer> al= new ArrayList<Integer>();
 		// Print the number of 2s that divide n
 		while (n % 2 == 0) {
-			System.out.print(2 + " ");
+			//System.out.print(2 + " ");
+			al.add(2);
 			n /= 2;
 		}
 		// n must be odd at this point. So we can
@@ -20,14 +26,18 @@ public class PrimeFactorization {
 		for (int i = 3; i <= Math.sqrt(n); i += 2) {
 			// While i divides n, print i and divide n
 			while (n % i == 0) {
-				System.out.print(i + " ");
+			//	System.out.print(i + " ");
+				al.add(i);
 				n /= i;
 			}
 		}
 		// This condition is to handle the case whien
 		// n is a prime number greater than 2
 		if (n > 2)
-			System.out.print(n);
+			//System.out.print(n);
+			al.add(n);
+		
+		return al;
 	}
 
 }
